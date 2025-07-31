de984dfb241adcb1c42c6b3e9eceb9b1e71dd730
Ivan Posazhennikov <126297410+AvinFlower@users.noreply.github.com>
2025-07-31 14:00:39 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        disko.diskoModules.disko
+        disko.nixosModules.disko
         ./disko.nix
         ./configuration.nix 
         ];
