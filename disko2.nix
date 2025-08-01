8226e85510f199637393fe8e98c7af8a53284e0f
Ivan Posazhennikov <126297410+AvinFlower@users.noreply.github.com>
2025-08-01 11:54:01 +0300

    Update disko2.nix

--- disko2.nix
+++ disko2.nix
@@ -1,51 +1,46 @@
 {
-  disko.devices = {
-    disk = {
-      main = {
-        device = "/dev/nvme0n1";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              index = 1;
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
-            };
-            root = {
-              index = 2;
-              end = "-6G";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-            encryptedSwap = {
-              index = 3;
-              size = "10M";
-              content = {
-                type = "swap";
-                randomEncryption = true;
-                priority = 100; # prefer to encrypt as long as we have space for it
-              };
-            };
-            plainSwap = {
-              index = 4;
-              label = "swap";
-              size = "100%";
-              content = {
-                type = "swap";
-                discardPolicy = "both";
-                resumeDevice = true; # resume from hiberation from this device
-              };
-            };
+  disko.devices.disk.main = {
+    device = "/dev/vda";
+    type = "disk";
+    content = {
+      type = "gpt";
+      partitions = {
+        ESP = {
+          index = 1;
+          size  = "512M";
+          type  = "EF00";
+          content = {
+            type         = "filesystem";
+            format       = "vfat";
+            mountpoint   = "/boot";
+            mountOptions = [ "umask=0077" ];
+          };
+        };
+        root = {
+          index = 2;
+          end   = "-2G";
+          content = {
+            type       = "filesystem";
+            format     = "ext4";
+            mountpoint = "/";
+          };
+        };
+        encryptedSwap = {
+          index = 3;
+          size  = "10M";
+          content = {
+            type             = "swap";
+            randomEncryption = true;
+            priority         = 100;
+          };
+        };
+        plainSwap = {
+          index = 4;
+          size  = "100%";
+          content = {
+            type           = "swap";
+            discardPolicy  = "both";
+            resumeDevice   = true;
           };
         };
       };
