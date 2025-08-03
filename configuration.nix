268c8f8095c28894eb227fe3ce002ddfae273da8
Ivan Posazhennikov <126297410+AvinFlower@users.noreply.github.com>
2025-08-03 13:28:24 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,70 +2,78 @@
 
 {
   imports = [ 
-  ./hardware-configuration.nix 
+    ./hardware-configuration.nix 
   ];
     
   system.stateVersion = "25.05";
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  networking.hostName = "nixos";
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
   time.timeZone = "Europe/Moscow";
-
-  # Включаем GNOME и X11
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "nvidia" ];
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-    xkb.layout = "us,ru";
-    xkb.options = "grp:win_alt_toggle";
-  };
     
   hardware = {
-    # Включаем графику (ранее opengl)
     graphics = {
-      enable    = true;
+      enable = true;
       enable32Bit = true;
     };
 
-    # Настройки NVIDIA
     nvidia = {
-      modesetting.enable     = true;
+      modesetting.enable = true;
       powerManagement.enable = true;
-      open                   = false;
-      nvidiaSettings         = true;
+      open = false;
+      nvidiaSettings = true;
       prime = {
-        sync.enable      = true;
-        nvidiaBusId      = "PCI:1:0:0";
-        intelBusId       = "PCI:0:2:0";
+        sync.enable = true;
+        nvidiaBusId = "PCI:1:0:0";
+        intelBusId = "PCI:0:2:0";
       };
     };
 
-    # Отключаем Bluetooth
     bluetooth.enable = false;
   };
         
-
-  # Остальные настройки...
-  services.printing.enable = true;
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    audio.enable = true;
-  };
+  services = {
+    xserver = {
+      enable = true;
+      videoDrivers = [ "nvidia" ];
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+      xkb.layout = "us,ru";
+      xkb.options = "grp:win_alt_toggle";
+    };
+    
+    displayManager.autoLogin = {
+      enable = true;
+      user = "vanish";
+    };
+    
+    printing.enable = true;
+    
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+      audio.enable = true;
+    };
+  };  
   
+  security.rtkit.enable = true;
   virtualisation.libvirtd.enable = true;
+
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
 
   users.users.vanish = {
     isNormalUser = true;
@@ -73,24 +81,14 @@
     initialPassword = "oxi-action";
   };
   
-  security.sudo = {
-    extraRules = [
-      { 
-        users = [ "vanish" ];
-        commands = [
-          { command = "ALL"; options = [ "NOPASSWD" ]; }  # Разрешить все команды
-        ];
-      }
-    ];
-  };
-
-  services.displayManager.autoLogin = {
-    enable = true;
-    user   = "vanish";
-  };
-  
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@tty1".enable = false;
+  security.sudo.extraRules = [
+    { 
+      users = [ "vanish" ];
+      commands = [
+        { command = "ALL"; options = [ "NOPASSWD" ]; }
+      ];
+    }
+  ];
   
   programs = {
     firefox.enable = true;
@@ -102,77 +100,78 @@
   };
   
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nixpkgs.config.allowUnfree = true;
-  # Разрешаем insecure-пакеты (freeimage требуется некоторым расширениям)
-  nixpkgs.config.permittedInsecurePackages = [
-    "freeimage-3.18.0-unstable-2024-04-18"
-  ];
-
-  environment.systemPackages = with pkgs; [
-    wget 
-    nix-search-cli 
-    git 
-    vscode 
-    python3 
-    v2rayn 
-    onlyoffice-bin
-    telegram-desktop
-    p7zip 
-    anydesk 
-    xterm 
-    popsicle
-    pavucontrol
-    virt-manager
-    autorandr
-    python3Full
-    neofetch
-    mangohud
-    protonup
-    lutris
-    bottles
-    
-    
-    gnomeExtensions.appindicator
-    gnomeExtensions.gsconnect
-    gnomeExtensions.translate-clipboard
-    gnomeExtensions.brightness-control-using-ddcutil
-    
-    gnomeExtensions.dock-from-dash
-    gnomeExtensions.blur-my-shell
-    gnomeExtensions.panel-corners
-    gnomeExtensions.arcmenu
-    gnomeExtensions.compiz-windows-effect
-    gnomeExtensions.burn-my-windows
-    gnomeExtensions.desktop-cube
-    gnomeExtensions.just-perfection
-    gnomeExtensions.quick-settings-tweaker
-    gnomeExtensions.forge
-    gnomeExtensions.pop-shell  
-  ];
   
-  environment.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
-      "\${HOME}/.steam/root/compatibilitytools.d";
+  nixpkgs.config = {
+    allowUnfree = true;
+    permittedInsecurePackages = [
+      "freeimage-3.18.0-unstable-2024-04-18"
+    ];
   };
-  
-  environment.gnome.excludePackages = with pkgs; [
-    #baobab      # disk usage analyzer
-    cheese      # photo booth
-    eog         # image viewer
-    epiphany    # web browser
-    #gedit       # text editor
-    simple-scan # document scanner
-    totem       # video player
-    yelp        # help viewer
-    evince      # document viewer
-    file-roller # archive manager
-    geary       # email client
-    seahorse    # password manager
-    gnome-tour
 
-    # these should be self explanatory
-    gnome-calendar gnome-characters gnome-contacts
-    gnome-maps gnome-music gnome-photos
-    gnome-weather pkgs.gnome-connections
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      wget 
+      nix-search-cli 
+      git 
+      vscode
+      v2rayn 
+      onlyoffice-bin
+      telegram-desktop
+      p7zip 
+      anydesk 
+      xterm 
+      popsicle
+      pavucontrol
+      virt-manager
+      autorandr
+      python3Full
+      neofetch
+      mangohud
+      protonup
+      lutris
+      bottles
+    
+      gnomeExtensions.appindicator
+      gnomeExtensions.gsconnect
+      gnomeExtensions.translate-clipboard
+      gnomeExtensions.brightness-control-using-ddcutil
+      gnomeExtensions.dock-from-dash
+      gnomeExtensions.blur-my-shell
+      gnomeExtensions.panel-corners
+      gnomeExtensions.arcmenu
+      gnomeExtensions.compiz-windows-effect
+      gnomeExtensions.burn-my-windows
+      gnomeExtensions.desktop-cube
+      gnomeExtensions.just-perfection
+      gnomeExtensions.quick-settings-tweaker
+      gnomeExtensions.forge
+      gnomeExtensions.pop-shell  
+    ];
+    
+    sessionVariables = {
+      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
+    };
+    
+    gnome.excludePackages = with pkgs; [
+      cheese
+      eog
+      epiphany
+      simple-scan
+      totem
+      yelp
+      evince
+      file-roller
+      geary
+      seahorse
+      gnome-tour
+      xterm
+      gnome-characters
+      gnome-contacts
+      gnome-maps
+      gnome-music
+      gnome-photos
+      gnome-weather
+      gnome-connections
+    ];
+  };
 }
