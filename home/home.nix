c4f7cbf1acadddcc421096ebd673e2a28069fd2d
Ivan Posazhennikov <126297410+AvinFlower@users.noreply.github.com>
2025-08-08 17:53:49 +0300

    Update home.nix

--- home/home.nix
+++ home/home.nix
@@ -13,28 +13,32 @@
     # ".config/qbittorrent/qbittorrent.conf".source = ./configs/qbittorrent.conf;
     # Добавляй сюда остальные конфиги по мере необходимости
 
-    ".config/mimeapps.list".text = ''
-      [Default Applications]
-      text/plain=code.desktop
-      text/csv=code.desktop
-      text/x-log=code.desktop
-      text/x-ini=code.desktop
-      application/x-msdos-program=code.desktop
-      application/rtf=code.desktop
-      text/html=code.desktop
-      text/xml=code.desktop
-      application/json=code.desktop
-      text/x-yaml=code.desktop
-      text/x-sql=code.desktop
-      application/javascript=code.desktop
-      text/x-vbscript=code.desktop
-      text/x-php=code.desktop
-      text/x-python=code.desktop
-      application/x-sh=code.desktop
-      application/x-shellscript=code.desktop
-      application/x-powershell=code.desktop
-      text/x-tex=code.desktop
-    '';
+  ".config/mimeapps.list".text = ''
+    [Default Applications]
+    text/plain=code.desktop
+    text/csv=code.desktop
+    text/x-log=code.desktop
+    text/x-ini=code.desktop
+    application/x-msdos-program=code.desktop
+    application/rtf=code.desktop
+    text/html=code.desktop
+    text/xml=code.desktop
+    application/json=code.desktop
+    text/x-yaml=code.desktop
+    text/x-sql=code.desktop
+    application/javascript=code.desktop
+    text/x-vbscript=code.desktop
+    text/x-php=code.desktop
+    text/x-python=code.desktop
+    application/x-sh=code.desktop
+    application/x-shellscript=code.desktop
+    application/x-powershell=code.desktop
+    text/x-tex=code.desktop
+    text/x-nix=code.desktop
+    text/plain=code.desktop
+    text/x-conf=code.desktop
+    text/x-lock=code.desktop
+  '';
   };
 
   
