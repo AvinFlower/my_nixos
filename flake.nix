06ae6c16f20caa8a4115963c11b6bb23a4b04113
Ivan Posazhennikov <126297410+AvinFlower@users.noreply.github.com>
2025-08-08 17:46:16 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,17 +1,31 @@
 {
-  description = "NixOS Flake+Disko";
+  description = "NixOS configuration with Home Manager";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    disko.url = "github:nix-community/disko";
-  };
-  outputs = { self, nixpkgs, disko, ...}: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        disko.nixosModules.disko
-        ./disko.nix
-        ./configuration.nix 
-        ];
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.vanish = import ./home/home.nix;
+            home-manager.backupFileExtension = "backup";
+          }
+        ];
+      };
+    };
 }
